261ca7b5ccffc31dbe62cfc95f4444c517431df2
Tobias Northrup <tobias-northrup@uiowa.edu>
2023-03-04 16:31:19 -0600

    created the chart component for the dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import SideNavBar from './components/SideNavBar';
 import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-    <div className="">
-      <Dashboard/>      
-    </div>
+    <Dashboard/>      
   );
 }
 
