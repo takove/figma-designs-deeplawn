1c638968ac8f97b54575e32d8be412390c2d3dd6
Tobias Northrup <tobias-northrup@uiowa.edu>
2023-03-04 17:54:07 -0600

    added search bar and moved some table code to the table page

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,24 +1,51 @@
 import React from 'react';
 import SideNavBar from '../components/SideNavBar';
+import SearchBar from "../components/SearchBar";
+import TableBody from "../components/TableBody";
 
 export default function Search () {
-    const search = (
-        <div>
-          <main className="flex-1">
-            <div className="py-6">
-              <div className=" max-w-full px-4 sm:px-6 lg:px-8">
-                <h1 className="text-2xl font-bold text-gray-900">Search</h1>
-              </div>
-              <div className=" max-w-full px-4 sm:px-6 lg:px-8">
-                {/* Replace with your content */}
     
-                
-    
-                {/* End Replace */}
-              </div>
+  const table = [
+    {address:"2124 S St Marys St, Siou...", ownersName:"Joel Northup", date:"24th Apr 2022", amountPaid:"$2399", type:"Estimate sent"}, 
+    {address:"2124 S St Marys St, Siou...", ownersName:"Joel Northup", date:"24th Apr 2022", amountPaid:"$2399", type:"Estimate sent"}, 
+    {address:"2124 S St Marys St, Siou...", ownersName:"Joel Northup", date:"24th Apr 2022", amountPaid:"$2399", type:"Estimate sent"}, 
+    {address:"2124 S St Marys St, Siou...", ownersName:"Joel Northup", date:"24th Apr 2022", amountPaid:"$2399", type:"Estimate sent"}, 
+    {address:"2124 S St Marys St, Siou...", ownersName:"Joel Northup", date:"24th Apr 2022", amountPaid:"$2399", type:"Estimate sent"}, 
+    {address:"2124 S St Marys St, Siou...", ownersName:"Joel Northup", date:"24th Apr 2022", amountPaid:"$2399", type:"Estimate sent"}, 
+    {address:"2124 S St Marys St, Siou...", ownersName:"Joel Northup", date:"24th Apr 2022", amountPaid:"$2399", type:"Estimate sent"}, 
+    {address:"2124 S St Marys St, Siou...", ownersName:"Joel Northup", date:"24th Apr 2022", amountPaid:"$2399", type:"Estimate sent"}, 
+    {address:"2124 S St Marys St, Siou...", ownersName:"Joel Northup", date:"24th Apr 2022", amountPaid:"$2399", type:"Estimate sent"}, 
+    {address:"2124 S St Marys St, Siou...", ownersName:"Joel Northup", date:"24th Apr 2022", amountPaid:"$2399", type:"Estimate sent"}, 
+    {address:"2124 S St Marys St, Siou...", ownersName:"Joel Northup", date:"24th Apr 2022", amountPaid:"$2399", type:"Estimate sent"}, 
+    {address:"2124 S St Marys St, Siou...", ownersName:"Joel Northup", date:"24th Apr 2022", amountPaid:"$2399", type:"Estimate sent"}, 
+    {address:"2124 S St Marys St, Siou...", ownersName:"Joel Northup", date:"24th Apr 2022", amountPaid:"$2399", type:"Estimate sent"}, 
+    {address:"2124 S St Marys St, Siou...", ownersName:"Joel Northup", date:"24th Apr 2022", amountPaid:"$2399", type:"Estimate sent"}, 
+    {address:"2124 S St Marys St, Siou...", ownersName:"Joel Northup", date:"24th Apr 2022", amountPaid:"$2399", type:"Estimate sent"}, 
+    {address:"2124 S St Marys St, Siou...", ownersName:"Joel Northup", date:"24th Apr 2022", amountPaid:"$2399", type:"Estimate sent"}, 
+    {address:"2124 S St Marys St, Siou...", ownersName:"Joel Northup", date:"24th Apr 2022", amountPaid:"$2399", type:"Estimate sent"}
+  ]
+
+  const search = (
+    <div>
+      <main className="flex-1">
+        <div className="py-6">
+          <div className=" max-w-full px-4 sm:px-6 lg:px-8">
+            <h1 className="text-2xl font-bold text-gray-900">Search</h1>
+          </div>
+          <div className=" max-w-full px-4 sm:px-6 lg:px-8">
+            {/* Replace with your content */}
+            <div className=" max-w-full px-4 sm:px-6 lg:px-2 lg:py-2">
+              <h1 className="text-2xl font-bold text-gray-900">
+                Active Orders
+              </h1>
             </div>
-          </main>
+            <SearchBar/>
+            <TableBody table={table}/>
+            {/* End Replace */}
+          </div>
         </div>
-      );
-      return <SideNavBar page={search} />;
+      </main>
+    </div>
+  );
+  return <SideNavBar page={search} />;
 }
