1bbc05fb48bcbb79eff34ac50a535932c70775b0
Tobias Northrup <tobias-northrup@uiowa.edu>
2023-03-10 23:03:21 -0600

    added accounts page and tabs

--- src/components/ProgramCards.tsx
+++ src/components/ProgramCards.tsx
@@ -47,26 +47,40 @@ export default function ProgramCards() {
             key={person.email}
             className="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-green-500 focus-within:ring-offset-2 hover:border-gray-400"
           >
-            <div className="flex-shrink-0">
+            {/* <div className="flex-shrink-0">
               <img className="h-20 w-20 rounded-lg" src={person.image} alt="" />
-            </div>
-            <div className="flex justify-center">
-                <div>
-                    <div className="mb-4">
-                    <input
-                        className="mt-[0.3rem] mr-2 h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-[rgba(0,0,0,0.25)] outline-none before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-white after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s]"
-                        type="checkbox"
-                        role="switch"
-                        id="flexSwitchCheckDefault01" />
-                    </div>
+            </div> */}
+            <div className="max-w-sm rounded overflow-hidden shadow-lg">
+              <img className="w-full" src={person.image} alt="Sunset in the mountains"/>
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+                <p className="text-gray-700 text-base">
+                  This is that
+                </p>
+              </div>
+              <div className="flex flex-col-2 px-6 pt-4 pb-2">
+                <div className="min-w-0 flex-1">
+                  <a href="#" className="focus:outline-none">
+                    <span className="absolute inset-0" aria-hidden="true" />
+                    <p className="text-sm font-medium text-gray-900">{person.name}</p>
+                    <p className="truncate text-sm text-gray-500">{person.role}</p>
+                  </a>
                 </div>
-            </div>
-            <div className="min-w-0 flex-1">
-              <a href="#" className="focus:outline-none">
-                <span className="absolute inset-0" aria-hidden="true" />
-                <p className="text-sm font-medium text-gray-900">{person.name}</p>
-                <p className="truncate text-sm text-gray-500">{person.role}</p>
-              </a>
+                <div className="flex justify-center">
+                  <div>
+                      <div className="mb-4">
+                      <input
+                          className="mt-[0.3rem] mr-2 h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-[rgba(0,0,0,0.25)] outline-none before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-white after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s]"
+                          type="checkbox"
+                          role="switch"
+                          id="flexSwitchCheckDefault01" />
+                      </div>
+                  </div>
+                </div>
+                {/* <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span> */}
+              </div>
             </div>
           </div>
         ))}
---
+++ src/pages/Accounts.tsx
@@ -0,0 +1,131 @@
+import React, { useState, useEffect} from 'react';
+import SideNavBar from '../components/SideNavBar';
+import Payments from "../components/Payments";
+import DateDropdown from "../components/DateDropdown";
+import BillingHistory from "../components/BillingHistory";
+import PaymentInfo from "../components/PaymentInfo";
+import Account from "../components/Account";
+import Tabs from "../components/Tabs";
+
+export default function Accounts() {
+  const [open, setOpen] = useState(false)
+  const tabs = [
+    { name: 'Payments', href: '#', current: false },
+    { name: 'Account', href: '#', current: false },
+    { name: 'Payment Info', href: '#', current: true },
+    { name: 'Billing History', href: '#', current: true },
+  ]
+  const [activeTab, setActiveTab] = useState<any>(tabs[0].name);
+  useEffect(() => {
+    setActiveTab(activeTab);
+    console.log("test")
+  }, [activeTab]);
+
+  const table = [
+    {type:"2124 S St Marys St, Siou...", total:"Joel Northup", usage:"24th Apr 2022"}, 
+    {type:"2124 S St Marys St, Siou...", total:"Joel Northup", usage:"24th Apr 2022"}, 
+    {type:"2124 S St Marys St, Siou...", total:"Joel Northup", usage:"24th Apr 2022"}, 
+    {type:"2124 S St Marys St, Siou...", total:"Joel Northup", usage:"24th Apr 2022"}, 
+    {type:"2124 S St Marys St, Siou...", total:"Joel Northup", usage:"24th Apr 2022"}, 
+    {type:"2124 S St Marys St, Siou...", total:"Joel Northup", usage:"24th Apr 2022"}, 
+  ]
+  const table2 = [
+    {Invoice:"2124 S St Marys St, Siou...", Date:"24th Apr 2022", Amount:"100"}, 
+    {Invoice:"2124 S St Marys St, Siou...", Date:"24th Apr 2022", Amount:"111"}, 
+    {Invoice:"2124 S St Marys St, Siou...", Date:"24th Apr 2022", Amount:"111"}, 
+    {Invoice:"2124 S St Marys St, Siou...", Date:"24th Apr 2022", Amount:"111"}, 
+    {Invoice:"2124 S St Marys St, Siou...", Date:"24th Apr 2022", Amount:"111"}, 
+    {Invoice:"2124 S St Marys St, Siou...", Date:"24th Apr 2022", Amount:"222"}, 
+  ]
+  const accounts = (
+    <div>
+      <main className="flex-1">
+        <div className="py-6">
+          <div className=" max-w-full px-4 sm:px-6 lg:px-8">
+            <h1 className="text-2xl font-bold text-gray-900">Hello {"Accounts"}</h1>
+          </div>
+          <div className=" max-w-full px-4 sm:px-6 lg:px-8">
+            {/* Replace with your content */}
+            <Tabs 
+            tabs={tabs} 
+            activeTab={activeTab}
+            setActiveTab={setActiveTab}
+            />
+            {activeTab === tabs[0].name ? (
+                <div>
+                    <div className="py-6">
+                      Payments
+                    </div>
+                    <Payments table={table}/>
+                    {/* <ImportButton 
+                    open={open}
+                    setOpen={setOpen}
+                    />
+                    <SearchButton
+                    open={open}
+                    setOpen={setOpen}
+                    />
+                    <ServiceCards/> */}
+                </div>
+            ) : activeTab === tabs[1].name ? (
+                <div>
+                    <div className="py-6">
+                      Account
+                    </div>
+                    <Account/>
+                    {/* <SearchBar/>
+                    <ImportButton
+                    open={open}
+                    setOpen={setOpen}
+                    />
+                    <SearchButton
+                    open={open}
+                    setOpen={setOpen}
+                    />
+                    <ProgramCards/> */}
+                </div>
+            ): activeTab === tabs[2].name ? (
+              <div>
+                  <div className="py-4">
+                    Payment Info
+                  </div>
+                  <PaymentInfo/>
+                  {/* <SearchBar/>
+                  <ImportButton
+                  open={open}
+                  setOpen={setOpen}
+                  />
+                  <SearchButton
+                  open={open}
+                  setOpen={setOpen}
+                  />
+                  <Discounts/> */}
+              </div>
+            ) : activeTab === tabs[3].name ? (
+              <div>
+                <div className="py-6">
+                  Billing History
+                </div>
+                
+                  <DateDropdown/>
+                  <BillingHistory table={table2}/>
+                  {/* <SearchBar/>
+                  <ImportButton
+                  open={open}
+                  setOpen={setOpen}
+                  />
+                  <SearchButton
+                  open={open}
+                  setOpen={setOpen}
+                  />
+                  <Discounts/> */}
+              </div>
+            ) : null }
+            {/* End Replace */}
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+  return <SideNavBar page={accounts} />;
+}
